00a121959c1cf28afdcefb3b4c748bc0415c948b
aleksandr bogatikov <perlexed@gmail.com>
2020-02-23 11:33:15 +0700

    initial commit, excluded IDE directory

---
+++ Assets/Scripts/BagController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BagController : MonoBehaviour
+{
+    private bool _isDragging = false;
+    public bool canBeRotated;
+
+    private bool CanBeDropped()
+    {
+        return true;
+    }
+
+    public void OnMouseDown()
+    {
+        if (!_isDragging)
+        {
+            _isDragging = true;
+            return;
+        }
+
+        if (CanBeDropped())
+        {
+            _isDragging = false;
+            PlaceOfShelf();
+        }
+    }
+
+    void PlaceOfShelf()
+    {
+        Debug.Log("placed");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isDragging)
+        {
+            Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            transform.position = new Vector2(cursorPosition.x, cursorPosition.y);
+
+        }
+    }
+}
